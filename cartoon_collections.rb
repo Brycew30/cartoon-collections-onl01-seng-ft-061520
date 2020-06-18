b8547eb253fc78a9e04dc61caee51dd3ddd70353
Bryce Whittington <brycew30@gmail.com>
2020-06-18 19:16:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,8 @@ end
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map! {|calls|
-  names.map! {|name| name.capitalize }
+  planeteer_calls.map! {|calls| calls.capitalize}
+
 end
 
 def long_planeteer_calls# code an argument here

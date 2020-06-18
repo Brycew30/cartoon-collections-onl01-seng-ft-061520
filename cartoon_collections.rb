dd2e9ca834683522cbc6d8e3b0006133d98b211c
Bryce Whittington <brycew30@gmail.com>
2020-06-18 19:08:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,15 +4,15 @@ def roll_call_dwarves(dwarves)
     puts "#{index} #{dwarf}"
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+#summon_captain_planet# code an argument here
+#Your code here
+#
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+#long_planeteer_calls# code an argument here
+#Your code here
+#
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+#find_the_cheese# code an argument here
+#the array below is here to help
+#eese_types = ["cheddar", "gouda", "camembert"]
+#
